732ce121f95d094bf43d3bedd1d28ef5a4198144
Damir <90550769+DamirKul@users.noreply.github.com>
2023-11-22 16:08:24 +0300

    first change

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # cybersport-news-parser
+
+Collecting information (title, publish date and link) about last news from cybersport.ru using rss feed. Collected data is saved in a .csv file.
+
+## Data sources
+* Cybersport.ru rss feed: https://www.cybersport.ru/rss/materials
+
+## Requirements
+* Python
+
+## Launch
+Run the file:
+
+* 'parse.py'
--- parse.py
+++ parse.py
@@ -1,24 +1,38 @@
-from bs4 import BeautifulSoup
-import requests
 import csv
 
-url = requests.get('https://www.cybersport.ru/rss/materials')
+from bs4 import BeautifulSoup
+import requests
 
-if url.status_code != 200:
-    raise Exception(f'Error code {url.status_code}')
-
-
-soup = BeautifulSoup(url.content, 'xml')
-items = soup.find_all('item')
+URL_ADDRESS = 'https://www.cybersport.ru/rss/materials'
 
 
-with open('output.csv', 'w', encoding='UTF-8', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
-    
-    for item in items:
+def parse_cybersport() -> None:
+    try:
+        request = requests.get(URL_ADDRESS, timeout=(5, 5))
+        request.raise_for_status()
+        soup = BeautifulSoup(request.content, 'xml')
+        items = soup.find_all('item')
 
-        title = item.title.text
-        date = item.pubDate.text
-        link = item.link.text
+        with open('output.csv', 'w', encoding='UTF-8', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=' ',
+                                quoting=csv.QUOTE_MINIMAL)
+
+            for item in items:
+                title = item.title.text
+                date = item.pubDate.text
+                link = item.link.text
+                writer.writerow([title, date, link])
         
-        writer.writerow([title, date, link])
+        print('Success!')
+    except requests.exceptions.HTTPError as errh:
+        print(errh.args[0])
+    except requests.exceptions.ConnectTimeout as errct:
+        print(errct.args[0])
+    except requests.exceptions.ReadTimeout as errrt:
+        print(errrt.args[0])
+    except requests.exceptions.RequestException: 
+        print('Request exception occured.')
+
+
+if __name__ == '__main__':
+    parse_cybersport()
